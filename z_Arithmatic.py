123e797cc741074b2eeef0b4c7c60f454d7a53dd
Optimus3264 <bhushanpatil096@gmail.com>
2025-03-22 20:30:02 +0530

    Add files via upload

---
+++ z_Arithmatic.py
@@ -0,0 +1,31 @@
+#Define two numbers
+a=5
+b=10
+
+#Addition
+addition=a+b
+print(f'Addition={a}+{b}={addition}')
+
+#Substraction
+substraction=a-b
+print(f'Substraction={a}-{b}={substraction}')
+
+#Multiplication
+multiplication=a*b
+print(f'multiplication={a}x{b}={multiplication}')
+
+#Division
+division=a/b
+print(f'division={a}/{b}={division}')
+
+#Floor Division(Integer Division)
+FloorDivision=a//b
+print(f'FloorDivision={a}//{b}={FloorDivision}')
+
+#Modulus(Reminder)
+Modulus=a%b
+print(f'Modulus={a}%{b}={Modulus}')
+
+#Exponention(Power)
+Exponention=a**b
+print(f'Exponention={a}**{b}={Exponention}')
