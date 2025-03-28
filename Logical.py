ca6e70f79b5481b230b91c30e6d226dbf1afe1af
Optimus3264 <bhushanpatil096@gmail.com>
2025-03-28 22:55:04 +0530

    Add files via upload

---
+++ Logical.py
@@ -0,0 +1,16 @@
+#Variables for demonstration
+a=True
+b=False
+
+print('a=',a)
+print('b=',b)
+
+#AND
+print('a and b=', a and b)
+
+#OR
+print('a or b=', a or b)
+
+#NOT
+print('not a=', not a) #negates the value of a
+print('not b=', not b) #negates the value of b
