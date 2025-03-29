aef6afe1b8b3b19559411142b2c2a43c5de9300a
Optimus3264 <bhushanpatil096@gmail.com>
2025-03-29 22:33:09 +0530

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,39 @@
+a=10
+
+#Assign
+print(f'Initial value of a = {a}')
+
+
+#Add and Assign
+a+=5
+print(f'value of a after a+= {a}')
+
+
+#Substract and Assign
+a-=3
+print(f'value of a after a-= {a}')
+
+
+#Multiply and Assign
+a*=2
+print(f'value of a after a*= {a}')
+
+
+#Divide and Assign
+a/=4
+print(f'value of a after a/= {a}')
+
+
+#Modulus and Assign  (Modulus=remainder after division)
+a%=4
+print(f'value of a after a%= {a}')
+
+
+#Floor division and Assign (Floor division=division excluding remainder)
+a//=2
+print(f'value of a after a//= {a}')
+
+
+#Exponention after Assign (Exponention=to the power of)
+a**=3
+print(f'value of a after a**= {a}')
