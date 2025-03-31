153d90915c696e255210ff314b4c01efce56b66b
Optimus3264 <bhushanpatil096@gmail.com>
2025-03-31 23:34:34 +0530

    Add files via upload

---
+++ If Condition.py
@@ -0,0 +1,23 @@
+#Basic structure
+#Code to run if condition is true
+
+#1 If
+x=10
+if x>5:
+    print('x is greater than 5')
+
+
+
+#2 If, Elif(else if) and Else
+if x<10:
+    print('x is less than 10')
+elif x>10:
+    print('x is greater than 10')
+else:
+    print('x is equal to 10')
+
+
+
+#3 If, And
+if x>5 and x<11:
+    print('both the conditions are true')
