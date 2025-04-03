3a1efd9f45d1f24dd854730795afe532c13c6652
Optimus3264 <bhushanpatil096@gmail.com>
2025-04-03 22:33:14 +0530

    Add files via upload

---
+++ Number_status.py
@@ -0,0 +1,8 @@
+n=float(input('Enter a number:'))
+
+if n>0:
+    print('This number is Positive')
+elif n<0:
+    print('This number is Negative')
+else:
+    print('This number is equal to Zero') 
