0667f8547b1c03054c460c777088b47700cefbd3
Optimus3264 <bhushanpatil096@gmail.com>
2025-09-09 21:58:31 +0530

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,20 @@
+def add (a,b):
+    return  a+b
+result=add(10,10)
+
+print(result)
+
+def substract (c,d):
+    return c-d
+result=substract(10,10)
+print(result)
+
+def multiply(a,b):
+    return a*b
+result=multiply(10,10)
+print(result)
+
+def divide (a,b):
+    return a/b
+result=divide(10,5)
+print(result)
