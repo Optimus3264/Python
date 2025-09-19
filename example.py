afb946588da78e1ef2f7d04bd20da7d414c0acef
Optimus3264 <bhushanpatil096@gmail.com>
2025-09-19 22:38:06 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,100 @@
+#def even_odd(num):
+ #   if num%2==0:
+  #      return f'{num} is even'
+   #else:
+    #    return f'{num} is odd'
+#print(even_odd(5))
+
+#def add(a,b):
+ #   return a+ b
+#result=add(5,3)
+#print(result)
+
+
+#def substract(a,b):
+   # return a-b
+#result=substract(5,3)
+#print(result)
+
+#def multiply(a,b):
+   # return a* b
+#result=multiply(5,3)
+#print(result)
+
+#def divide(a,b):
+ #   return a/b
+#result=divide(10,2)
+#print(result)
+
+#def divide(a,b):
+ #   return a// b
+#result=divide(5,2)
+#print(result)
+
+#def greet():
+   # print('hello,i am rushikesh')
+#greet()
+
+#def greet(name):
+    #return f'{name}, here '
+#print(greet('rushikesh'))
+
+#modulus
+#c=5
+#d=3
+#modulus= c%d
+#print(f'modulus:{c}%{d}={modulus}')
+
+#expoenatiation=c**d
+#print(f'expoenatiation:{c}**{d}= {expoenatiation}')
+
+#e=20
+#print(f'inital value:e={e}')
+
+#add and assign
+#e +=5
+#print(f'after e+=5:{e}')
+
+#substract and assign
+#e-=5
+#print(f' afetr e-=5:{e}')
+
+#multiply and assign
+#e*=5
+#print(f'after e*=5:{e}')
+
+#divide and assign
+#e/=5
+#print(f'after e/=5:{e}')
+
+#floor divide and assign
+#e//=4
+#print(f'after e//=4:{e}')
+
+#modulus and assign
+#e%=4
+#print(f'after e%=4:{e}')
+
+#exopnentiation and assign
+#e**=2
+#print(f'after e**=2:{e}')
+
+#fruit=['apple','banana','cherry']
+#for fruit in fruit:
+ #   print(fruit)
+
+#a=[1,2,3,4,5,6]
+#for i in range(5):
+ #   print(i)
+
+#a=2,4,6,8
+#for i in range(2,10,2):
+ # print(a)
+
+a='hello'
+#for letter in a:
+ #   print(a)
+
+for i in range(3):
+    for j in range(2):
+        print(a)
