c03f3d2f3897c79f3c7fb0dfcbd7fb4074b682fd
Optimus3264 <bhushanpatil096@gmail.com>
2025-09-20 22:30:39 +0530

    Add files via upload

---
+++ fruit.py
@@ -0,0 +1,65 @@
+fruit=['apple','banana','cherry','mango']
+#print(fruit)
+
+#slicing
+#print(fruit[0:2])
+#print(fruit[:2])
+#print(fruit[2:])
+
+#indexing
+#print(fruit[0])
+#print(fruit[-1])
+
+#access
+#print(fruit[3])
+
+#insert
+fruit.insert(0,'blueberry')
+#print(fruit)
+
+#remove
+fruit.remove('blueberry')
+#print(fruit)
+
+#append
+fruit.append('papaya')
+#print(fruit)
+
+#delete
+del fruit[4]
+#print(fruit)
+
+#change
+fruit[0]='berry'
+#print(fruit)
+
+#pop
+fruit.pop()
+#print(fruit)
+
+#sort
+#a=[0,3,1,2]
+#a.sort()
+#print(a)
+
+#reverse
+#a.reverse()
+#print(a)
+
+#loop
+#for fruits in fruit:
+    #print(fruit)
+
+#nested
+fruits=[['apple','cherry'],['orange','mango'],['lemon','papaya']]
+#print(fruits)
+
+#check item in list
+#print('cherry'in fruit)
+
+#length
+#print(len(fruit))
+
+#copy
+copy_item=fruit.copy()
+print(copy_item)
