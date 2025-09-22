52de74c17791e5b87d8d88209ea6168d4280f75e
Optimus3264 <bhushanpatil096@gmail.com>
2025-09-22 22:31:00 +0530

    Add files via upload

---
+++ list_item.py
@@ -0,0 +1,68 @@
+list=[1,2,3,4,5,6]
+#print(list)
+
+#slicing
+#print(list[:2])
+#print(list[0:3])
+#print(list[3:])
+
+#indexing
+#print(list[0])
+#print(list[-1])
+
+#access
+#print(list[2])
+
+#append
+list.append(7)
+#print(list)
+
+#insert
+list.insert(3,10)
+#print(list)
+
+#remove
+list.remove(10)
+#print(list)
+
+#change
+list[1]='apple'
+#print(list)
+
+#pop
+last_item=list.pop()
+#print(last_item)
+#print(list)
+
+#delete
+del list[1]
+#print(list)
+
+#sort
+a=[0,1,3,5,3,2,5,4]
+a.sort()
+#print(a)
+
+#reverse
+a.reverse()
+#print(a)
+
+#loop
+#for a in a:
+    #print(a)
+
+#check item in list
+#print(0 in list)
+
+#nested loop
+x =[[1,2],[3,4],[5,6]]
+#print(x)
+#print(x[0][1])
+
+#copy
+copy_list=x.copy()
+#print(copy_list)
+
+#length
+s=[0,1,2,3,4]
+print(len(s))
