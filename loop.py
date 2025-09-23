7a849c1fe03e18cf6c05ed590ef36612e93f5ec3
Optimus3264 <bhushanpatil096@gmail.com>
2025-09-23 22:45:26 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,28 @@
+
+#do something with item
+#loop through list
+#fruit=['apple','banana','cherry','graphs']
+#for fruit in fruit:
+#    print(fruit)
+
+#loop using range ()
+#print number 0 to 4
+#for i in range (5):
+  #  print(i)
+
+#lopp through range (start,stop,step)
+#print (1,10,1)
+#for i in range(1,10,2):
+ #   print(i)
+
+#loop through character in a string
+#a="hello welcome"
+#for letter in a:
+ #   print(letter)
+
+#nested for loop
+for i in range (2):
+    for j in range(3):
+        print(f'i={i},j={j}') 
+
+
