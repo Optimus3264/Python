f06e9875c118e2603dc7520e777f7c3d91f9c70d
Optimus3264 <bhushanpatil096@gmail.com>
2025-09-24 22:33:01 +0530

    Add files via upload

---
+++ mean,median,mode.py
@@ -0,0 +1,25 @@
+import statistics
+
+#data=[2,4,6,8,10,12,15]
+#mean=statistics.mean(data)
+#print('mean of data is:',mean)
+
+#median=statistics.median(data)
+#print('median od data is :',median)
+
+#mode=statistics.mode(data)
+#print('mode of data is:',mode)
+
+#a=[100,200,300,400,500,600,100]
+#mean=statistics.mean(a)
+#median=statistics.median(a)
+#mode=statistics.mode(a)
+
+#print('mean is:',mean)
+#print('median is:',median)
+#print('mode is:',mode)
+
+a=[1,2,3,4,5,6]
+print('mean of data is:',statistics.mean(a))
+print('median of data is :',statistics.median(a))
+print('mode of data is :',statistics.mode(a))
