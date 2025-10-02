701c04c8dd5ae581bdb62f8bcddaf4f24208b56a
Optimus3264 <bhushanpatil096@gmail.com>
2025-10-02 22:37:00 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,46 @@
+str1='hey'
+str2='python'
+#combinig string
+
+#result=str1 + ' ' +str2
+#print(result)
+
+#repitation
+str3='hey'
+repeated=str3*4
+#print(repeated)
+
+#Indexing
+str4='python'
+#print(str4[2])
+#print(str4[-2])
+
+#slicing
+str5='hey python'
+#print(str5[:2])
+#print(str5[2:])
+#print(str5[0:2])
+
+#length
+str6='gaurav'
+#print(len(str6))
+
+#change case
+a='hello'
+#print(a.upper())
+#print(a.title())
+#print(a.lower())
+#print(a.capitalize())
+
+#find substring
+a='hello world'
+#print(a.find('world'))
+#print(a.find('python'))
+
+#replace
+a='hey bhushan'
+#print(a.replace('hey','hi'))
+
+#trimming
+a='hey bhushan   '
+print(a.strip())-
