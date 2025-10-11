f3e25299f101aa27432a4aad7fbb02c44a551421
Optimus3264 <bhushanpatil096@gmail.com>
2025-10-11 22:15:49 +0530

    Add files via upload

---
+++ using array module.py
@@ -0,0 +1,10 @@
+import array
+arr = array.array('i'[1,2,3,4,5])  # 'i'stands for integer
+
+# Operations
+arr.append(6) #append value
+arr.insert(2,10) #insert at index 2
+arr.remove(4) #remove value
+print(arr.index(10)) #find index of value
+arr.reverse() #Reverse array
+
