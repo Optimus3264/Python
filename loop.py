31096daf187e5b2e0e896562bdf632e4d3a3d9cf
Optimus3264 <bhushanpatil096@gmail.com>
2025-10-13 22:12:08 +0530

    Add files via upload

--- loop.py
+++ loop.py
@@ -1,28 +1,28 @@
+#basic structure
+#for item in sequence
+    #do something with item
 
-#do something with item
-#loop through list
-#fruit=['apple','banana','cherry','graphs']
-#for fruit in fruit:
-#    print(fruit)
+#1. loop through a list
+fruits =['apple ', 'banana', 'cherry']
+for fruit in fruits:
+    print(fruit)
 
-#loop using range ()
-#print number 0 to 4
-#for i in range (5):
-  #  print(i)
+#2 loop using range()
+#prints number from 0 to 4
+for i in range(5):
+     print(i)
 
-#lopp through range (start,stop,step)
-#print (1,10,1)
-#for i in range(1,10,2):
- #   print(i)
+#3 loop with range(start,stop,step)
+#prints 2,4,6,8
+for i in range(2, 10, 2):
+    print(i)
 
-#loop through character in a string
-#a="hello welcome"
-#for letter in a:
- #   print(letter)
+#4 loop through characters in a string
+word = 'Hello'
+for letter in word:
+    print(letter)
 
-#nested for loop
-for i in range (2):
-    for j in range(3):
-        print(f'i={i},j={j}') 
-
-
+#5 Nested for loops
+for i in range(3):
+     for j in range(2):
+         print(f"i={i} , j={j}")
