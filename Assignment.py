f7ff4e0416941322b2a23d83f9400936f9a0e283
Optimus3264 <bhushanpatil096@gmail.com>
2025-10-19 23:50:27 +0530

    Add files via upload

--- Assignment.py
+++ Assignment.py
@@ -1,39 +1,35 @@
-a=10
+a = 10
 
 #Assign
-print(f'Initial value of a = {a}')
+print(f"initial value: a ={a}")
+
+#Add and assign
+a += 5 #equivalent to: a=a+ 5
+print(f"after a += 5 : {a}")
+
+#substract and assign
+a -=3 #equivalent to : a -3
+print(f"after a-=3 : {a}")
+
+#Multiply and assign 
+a *=2 #equivalent to a = a* 2
+print(f"after a *=2: {a}")
+
+#Divide and assign 
+a /= 4 #equivalent to : a = a/4
+print(f"after a/ = 4: {a}")
+
+#floor divide and assign 
+a //= 2 #equivalent a = a//2
+print(f"after a//=2:{a}")
+
+# Modulus and assign
+a %= 3  # Equivalent to: a = a % 3
+print(f"After a %= 3: {a}")
+
+# Exponentiation and assign
+a **= 2  # Equivalent to: a = a * 2
+print(f"After a **= 2: {a}")
 
 
-#Add and Assign
-a+=5
-print(f'value of a after a+= {a}')
 
-
-#Substract and Assign
-a-=3
-print(f'value of a after a-= {a}')
-
-
-#Multiply and Assign
-a*=2
-print(f'value of a after a*= {a}')
-
-
-#Divide and Assign
-a/=4
-print(f'value of a after a/= {a}')
-
-
-#Modulus and Assign  (Modulus=remainder after division)
-a%=4
-print(f'value of a after a%= {a}')
-
-
-#Floor division and Assign (Floor division=division excluding remainder)
-a//=2
-print(f'value of a after a//= {a}')
-
-
-#Exponention after Assign (Exponention=to the power of)
-a**=3
-print(f'value of a after a**= {a}')
