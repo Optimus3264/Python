8ec04584146d3dc31da1f41be5f3be5414be9bfb
Optimus3264 <bhushanpatil096@gmail.com>
2025-11-05 22:32:31 +0530

    Add files via upload

---
+++ string_test.py
@@ -0,0 +1,21 @@
+a='hello'
+b='world'
+c='ruShIkEsH PaTiL'
+#result=a+ " "+b
+#print(result)
+
+#print(c.upper())
+#print(c.lower())
+#print(c.capitalize())
+#print(c.title())
+
+#print(len(a))
+
+d=['hello','my' ,'world']
+#print(d.find('world'))
+#print(d.find('python'))
+
+#print(d.replace('world','kolhapur'))
+
+print(' '.join(d))
+
