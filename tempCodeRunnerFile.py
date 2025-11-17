cc834819dd2b6ab39b5be444b98d62a437986dcb
Optimus3264 <bhushanpatil096@gmail.com>
2025-11-17 22:46:16 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+x=int(input('enter 1st number'))
+y=int(input('enter 2nd number'))
+z=int(input('enter 3rd number'))
+
+if x>y and x<z :
+    print(f'enetr1st number')
+elif y>z:
+    print(f'enter 2nd number')
+else:
+    print(f'enter 3rd number')
