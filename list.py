838001db038a5864509bbeec823ec2922dcdc6e1
Optimus3264 <bhushanpatil096@gmail.com>
2025-11-18 22:52:59 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,55 @@
+
+my_list=[1,2,3,4,5]
+print(my_list)
+
+my_list=[10,20,30,40,50]
+print(my_list[2])
+print(my_list[4])
+
+list=[10,20,30,40,50]
+print(list[0:3])
+print(list[:2])
+print(list[::3])
+
+list=[1,2,3,4,5]
+list.append(6)
+print(list)
+
+list.insert(2,20)
+print(list)
+
+list.extend([7,8])
+print(list)
+
+list = [1,2,3,4,5]
+list.remove(3)
+print(list)
+
+lastitem=list.pop()
+print(lastitem)
+print(list)
+
+del list[2]
+print(list)
+
+list= [0,1,2,3,4,5,6,7,8,9,10]
+print(len(list))
+
+list=[4,5,6,2,3,1,7]
+m=list.sort()
+print(list)
+
+new_list=sorted(list,reverse=True)
+print(new_list)
+
+list=['honda','hero','bmw','ktm']
+print(list[2])
+print(list[:2])
+
+list.append('royal enfied')
+print(list)
+
+mix_list=[1,'banana','bike',60]
+print(mix_list)
+
+
